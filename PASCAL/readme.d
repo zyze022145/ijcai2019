f1ee08796b9cc6522e7f94c79595fad090d2c769
zyze022145 <49770476+zyze022145@users.noreply.github.com>
2019-04-20 15:50:05 +0800

    Create readme.d

---
+++ PASCAL/readme.d
@@ -0,0 +1,16 @@
+<div align=center><img src="PASCAL-5.jpg"/></div>
+<p align="center">
+  Figure 1: Some qualitative results of our model on PASCAL-5 datasets.
+</p>
+
+
+| Method      | IoU     | 
+:-:|:-:|
+| OSTC     | 0.216     |
+| OSLSM     | 0.326     |
+| ours     | 0.357     |
+
+<p align="center">
+  Table 1 : Our comparison with other methods on the PASCAL-5 datasets in terms of mean IoU. The setting of experiment is 
+  the same as our paper.
+</p>
